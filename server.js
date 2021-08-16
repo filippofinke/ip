42513c36d032245e5aadcf9e466a0c251f23d051
filippofinke <filippofinke@protonmail.ch>
2021-08-16 15:06:23 +0200

    ♻️ Entire code refactor

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
 const express = require("express");
+const geoip = require("geoip-country");
 const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 8080;
-const cloudflare = process.env.CLOUDFLARE === "true" || false;
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-	let response = {
-		ip: req.socket.remoteAddress,
-		country: null,
-	};
-
-	if (cloudflare) {
-		response.ip = req.header("CF-Connecting-IP");
-		response.country = req.header("CF-IPCountry");
-	}
-
-	return res.json(response);
+  return res.json({
+    ip: req.ip,
+    country: geoip.lookup(req.ip)?.country || null,
+  });
 });
 
 app.listen(port, () => {
-	console.log(`i app listening at http://localhost:${port}`);
-	if (cloudflare) {
-		console.log("! app behind Cloudflare");
-	}
+  console.log(`i app listening at http://localhost:${port}`);
 });
