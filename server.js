b4486df55a8ba96625ea2e3999acf50cef0953ef
Filippo Finke <filippofinke@protonmail.ch>
2021-08-16 18:55:22 +0200

    🐛 Trust reverse proxy

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.set("trust proxy", true);
+
 app.use(cors());
 
 app.get("/", (req, res) => {
-  return res.json({
-    ip: req.ip,
-    country: geoip.lookup(req.ip)?.country || null,
-  });
+	return res.json({
+		ip: req.ip,
+		country: geoip.lookup(req.ip)?.country || null,
+	});
 });
 
 app.listen(port, () => {
-  console.log(`i app listening at http://localhost:${port}`);
+	console.log(`i app listening at http://localhost:${port}`);
 });
